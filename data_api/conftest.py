d044071e91b4cc68c8005fc5fc840ed58bbb8e25
Yoonjae Park <paryoja@gmail.com>
2024-10-12 23:14:34 -0700

    initial commit

---
+++ data_api/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from data_api.users.models import User
+from data_api.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
